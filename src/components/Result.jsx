8077585af3a070d6c4bae95bc65f555ee0d2fbdb
ganeshshelkeGCS <91302142+ganeshshelkeGCS@users.noreply.github.com>
2024-06-03 15:41:13 +0530

    added Error Message

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -6,9 +6,11 @@ export default function Result({results}) {
         <div>
             {
                 results.map((result) => (
-                    <div key={result.is}>
-                        <h2>{result.overview}</h2>     
+                    <div key={result.id}>
+                        <h2>{result.original_title}</h2>  
+                        <hr />   
                     </div>
+                   
                 ))
             }
         </div>
