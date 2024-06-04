ba11ab3093c3ac35826bfcbd2c45ad146a541428
ganeshshelkeGCS <91302142+ganeshshelkeGCS@users.noreply.github.com>
2024-06-04 17:12:04 +0530

    Added Movie page

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'image.tmdb.org',
+            hostname: '**',
+            port:'',
             pathname: '**',
           },
         ],
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+ 'use client'
+
 import Link from 'next/link';
 import Image from 'next/image';
 import {FiThumbsUp} from 'react-icons/fi';
@@ -11,17 +13,29 @@ export default function Card({result}) {
                     width={500}
                     height={300}
                     className='sm:rounded-t-lg group-hover:opacity-85 transition-opacity duration-300'
+                    alt='loading.....'/>
+                
+                {/* <Image
+                   // src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                    loader={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                    src={`/next.svg`}
+                    width={500}
+                    height={300}
+                    className='sm:rounded-t-lg group-hover:opacity-85 transition-opacity duration-300'
                     alt='loading.....'>
-                </img>
-                <div className="">
+                </Image> */}
+
+                { <div className="">
                     <p className='line-clamp-2 text-md'>{result.overview}</p>
                     <h2 className='text-lg font-bold truncate'>{result.title || result.name}</h2>
                     <p className='flex gap-3 items-center'>
                         {result.release_date || result.first_air_date}
+                        
                         <FiThumbsUp />
                         {result.vote_count}
+                        <span>ID : {result.id}</span>
                     </p>
-                </div>
+                </div> }
             </Link>
         </div>
     )
